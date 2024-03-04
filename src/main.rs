1a92e6cf6d013ca98e64a7f81a424f2d7961f9fa
Alexander A.Rodin <@inbox.ru>
2024-03-04 19:06:01 +0300

    tuples, fmt+

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,21 @@
-#![crate_name = "learn_rust"]
 
-//! Not very bad for start
-//! - [x] it's easy
-//! - [ ] it works
-//!   - really?
-
-/// test struct
-#[allow(dead_code)]
-struct TestStruct {
-    /// test field
-    test_field: String,
-    /// pub field
-    pub pub_field: String,
-    /// not very pub pub
-    pub(self) super_pub_field: u8,
-}
-
-/// pub struct
-pub struct PubStruct(u8);
-
-/// struct
-#[derive(Debug)]
-struct SomeStruct(u8);
-
-/// not very pub pub struct
-#[allow(dead_code)]
-pub(self) struct SelfPubStruct(u8);
-
-
-#[derive(Debug)]
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
-
-/// main function as entry point
 fn main() {
-    println!("\nHell-0, world!");
-    
-    println!("31 = {}", PubStruct(31));
-    println!("13 = {:?}", PubStruct(13));
-    println!("?? --> {:?}", SomeStruct(3));
-
-    for color in [
-        Color { red: 128, green: 255, blue: 90 },
-        Color { red: 0, green: 3, blue: 254 },
-        Color { red: 0, green: 0, blue: 0 },
-    ].iter() {
-        println!( "{}", color );
-    }
+    let a_matrix = Matrix ( 1., 0.5, -0.5, 2. );
+    println!( "dbg\n{:?}\n", a_matrix );
+    println!( "show\n{}\n", a_matrix );
+    println!( "show\n{}\n", a_matrix.transpose() );
 }
 
+#[derive(Debug)]
+struct Matrix(f32,f32,f32,f32);
 
-impl core::fmt::Display for Color {
+impl core::fmt::Display for Matrix {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "RGB ({red}, {green}, {blue}) 0x{red:0>2X}{green:0>2X}{blue:0>2X}", 
-               red=self.red, green=self.green, blue=self.blue )
+        write!(f, "(\t{:+.2}\t{:+.2}\t)\n(\t{:+.2}\t{:+.2}\t)", self.0, self.1, self.2, self.3 )
     }
 }
-
-impl core::fmt::Display for PubStruct {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str( "testing" )?;
-        f.write_str( format!("<{}>", self.0).as_str() )?;
-        Ok(())
+impl Matrix {
+    fn transpose(&self) -> Self {
+        Self( self.0, self.2, self.1, self.3 )
     }
 }
-
-impl core::fmt::Debug for PubStruct {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str( "dbg\n" )?;
-        write!(f, "<{}>\n", self.0)
-    }
-}
-
