1459be5108d79a180c4e85f6fba45c8380c8bc25
Alexander A.Rodin <@inbox.ru>
2024-03-07 20:48:31 +0300

    learned ref and patterns

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 fn main() {
-    let my_collection = MyCollection{s:"324",i:0};
-    println!("dbg: {:?}", my_collection);
-    println!("attempt 1");
-    for item in my_collection {
-        println!(" item -> {}", item);
+    let mut arr = [
+        EnUm::Text("one".into()),
+        EnUm::Text("one2".into()),
+        EnUm::Point2 { x: 3, y: 4 },
+        EnUm::Text("two".into()),
+    ];
+    println!("\narr: {:?}\n", arr);
+    for item in &mut arr {
+        println!("");
+        match item {
+            EnUm::Point2 { x: ref mut vv, .. } => {
+                println!(" BEFORE -> {:?}", vv);
+                *vv = 111;
+                println!(" AFTER -> {:?}", vv);
+            }
+            ref val => {
+                println!(" a -> {:?}", val);
+            }
+        }
+        println!(" item2 -> {:?}", item);
     }
+    println!("\narr: {:?}\n", arr);
 }
 
 #[derive(Debug)]
-struct MyCollection {
-    s: &'static str,
-    i: usize,
-}
-
-impl Iterator for MyCollection {
-    type Item = char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result: Option<char> = self.s.chars().nth(self.i);
-        self.i += 1;
-        result
-    }
+enum EnUm {
+    Text(String),
+    Point2 { x: i32, y: i32 },
 }
